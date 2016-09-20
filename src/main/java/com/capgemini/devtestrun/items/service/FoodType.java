bebfe71f15d78a7b471bf1232e3831a9b1cca9a4
Nathan <nathan.orme23@gmail.com>
2016-09-20 11:54:48 +0100

    after stage 2 food type added
    Will check what type of food our items are

---
+++ src/main/java/com/capgemini/devtestrun/items/service/FoodType.java
@@ -0,0 +1,38 @@
+package com.capgemini.devtestrun.items.service;
+
+import java.util.ArrayList;
+
+public enum FoodType {
+
+	FREE("cold", "food"),
+    COLD_FOOD("cold", "food"),
+    WARM_FOOD("hot", "food");
+    
+    private ArrayList< String> types = new ArrayList<String>();
+    
+    /**
+     * Searches through the number
+     * of strings passed through the method
+     * and adds them to the array.
+     * @param triggers
+     */
+    private FoodType(String... fTypes) {
+        for (String type : fTypes) {
+        	types.add(type);
+        }
+    }
+    
+    public boolean triggeredBy(ArrayList<String> fTypes) {
+        boolean found = false;
+        for (String type : fTypes) {
+            if(types.contains(type)){
+            	found = true;
+            } else {
+            	found = false;
+                return found;
+            }
+        }
+        return found;
+    }
+	
+}
