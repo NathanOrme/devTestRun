a76c067a3683d5ddf40994fb9bfed9ca34476d50
Nathan <nathan.orme23@gmail.com>
2016-09-20 10:34:14 +0100

    after stage 1 initial set up of main app
    set up basic scanner for the system.

--- src/main/java/com/capgemini/devtestrun/App.java
+++ src/main/java/com/capgemini/devtestrun/App.java
@@ -1,13 +1,34 @@
 package com.capgemini.devtestrun;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	System.out.println("Menu:");
+		System.out.println("1.Cola: 50p");
+		System.out.println("2.Coffee: £1");
+		System.out.println("3.Cheese Sandwich: £2");
+		System.out.println("4.Steak Sandwich: £4.50");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Which item would you like?");
+
+		String input = in.nextLine();
+		while(!input.equalsIgnoreCase("x") || !input.contains("x")){
+			boolean exit = false;
+			System.out.println("Item processed");
+			if(exit){
+				System.out.println("Exiting system");
+			}
+		} 
+		if(input.equalsIgnoreCase("x")){
+			System.out.println("Exiting system");
+			in.close();
+		}
+		
     }
 }
