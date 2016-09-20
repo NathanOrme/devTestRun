6c9382758f7035b598e49a384fb04159217b0e05
Nathan <nathan.orme23@gmail.com>
2016-09-20 11:03:22 +0100

    after stage 1 added check for arguments passed through
    will treat arguments passed through main as a test run

--- src/main/java/com/capgemini/devtestrun/App.java
+++ src/main/java/com/capgemini/devtestrun/App.java
@@ -10,30 +10,33 @@ import java.util.Scanner;
  */
 public class App 
 {
-    public static void main( String[] args ){
-    	System.out.println("Menu:");
-		System.out.println("1.Cola: 50p");
-		System.out.println("2.Coffee: £1");
-		System.out.println("3.Cheese Sandwich: £2");
-		System.out.println("4.Steak Sandwich: £4.50");
-		Scanner in = new Scanner(System.in);
-		System.out.print("Which item would you like?");
-		List<String>entries = new ArrayList<String>();;
-		String input = in.nextLine();
-		while(!input.equalsIgnoreCase("x") || !input.contains("x")){
-			boolean exit = false;
-			entries.add(input);
-			System.out.println("Item processed");
-			System.out.println("Would you like to add another item? Press x if you wish to exit now");
-			if(exit){
+	public static void main( String[] args ){
+		if(args.length != 0){
+			System.out.println("Menu:");
+			System.out.println("1.Cola: 50p");
+			System.out.println("2.Coffee: £1");
+			System.out.println("3.Cheese Sandwich: £2");
+			System.out.println("4.Steak Sandwich: £4.50");
+			Scanner in = new Scanner(System.in);
+			System.out.print("Which item would you like?");
+			List<String>entries = new ArrayList<String>();;
+			String input = in.nextLine();
+			while(!input.equalsIgnoreCase("x") || !input.contains("x")){
+				boolean exit = false;
+				entries.add(input);
+				System.out.println("Item processed");
+				System.out.println("Would you like to add another item? Press x if you wish to exit now");
+				if(exit){
+					System.out.println("Exiting system");
+				}
+			} 
+			if(input.equalsIgnoreCase("x")){
+				System.out.println("Processing items added");
 				System.out.println("Exiting system");
+				in.close();
 			}
-		} 
-		if(input.equalsIgnoreCase("x")){
-			System.out.println("Processing items added");
-			System.out.println("Exiting system");
-			in.close();
+		} else {
+			System.out.println("Arguments passed through, test run activated");
 		}
-		
-    }
+	}
 }
